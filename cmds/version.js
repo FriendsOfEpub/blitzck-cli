9b66f3e4ef6bc61cb57730008cf4ed77af4791e2
JayPanoz <panozpublishingteam@gmail.com>
2018-09-06 18:38:06 +0200

    add logo in version cmd

--- cmds/version.js
+++ cmds/version.js
@@ -1,5 +1,25 @@
-const { version } = require("../package.json")
+const { version } = require("../package.json");
+const logo = `hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhhhhhhhyhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhhhhhh/+hhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhhhhs. yhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhhh/  .hhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhs.   /hhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhh/     shhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhs.     .hhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhh/       :ssssssyhhhhhhhhhh
+hhhhhhhhhhhhs.              +hhhhhhhhhhh
+hhhhhhhhhhh/              .shhhhhhhhhhhh
+hhhhhhhhhhyssssss:       +hhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhh.     .shhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhs     +hhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhh/   .shhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhh.  +hhhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhs .shhhhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhh++hhhhhhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhyhhhhhhhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh`;
 
 module.exports = () => {
-  console.log(`v${version}`)
+  console.log(`\n${logo} \n\nblitzck-cli v${version}\n`)
 }
